41e86ecba26554436060a225205e0639f1663753
shefalidk <53697671+shefalidk@users.noreply.github.com>
2019-08-18 13:06:45 +0530

    Add files via upload

---
+++ Echo-Service_Quickstart/apiman-quickstarts-master/release.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+echo ""
+echo "######################################"
+echo "  Releasing apiman quickstarts"
+echo "######################################"
+echo ""
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+echo "** Current Branch: $BRANCH **"
+echo ""
+
+RELEASE_VERSION=$1
+DEV_VERSION=$2
+
+if [ "x$RELEASE_VERSION" = "x" ]
+then
+  read -p "Release Version: " RELEASE_VERSION
+fi
+
+if [ "x$DEV_VERSION" = "x" ]
+then
+  read -p "New Development Version: " DEV_VERSION
+fi
+
+echo "######################################"
+echo "Release Version: $RELEASE_VERSION"
+echo "Dev Version: $DEV_VERSION"
+echo "######################################"
+
+mvn clean install
+STATUS=$?
+if [ $STATUS -eq 0 ]; then
+  echo "Build success!"
+else
+  echo "Build failed!"
+  exit 1
+fi
+
+
+mvn versions:set -DnewVersion=$RELEASE_VERSION
+find . -name '*.versionsBackup' -exec rm -f {} \;
+git add .
+git commit -m "Prepare for release $RELEASE_VERSION"
+git push origin $BRANCH
+
+mvn clean install
+
+git tag -a -m "Tagging release $RELEASE_VERSION" apiman-quickstarts-$RELEASE_VERSION
+git push origin apiman-quickstarts-$RELEASE_VERSION
+
+mvn deploy
+
+mvn versions:set -DnewVersion=$DEV_VERSION
+find . -name '*.versionsBackup' -exec rm -f {} \;
+git add .
+git commit -m "Update to next development version: $DEV_VERSION"
+git push origin $BRANCH
